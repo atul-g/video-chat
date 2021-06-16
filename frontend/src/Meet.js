e36bc0d7ef72634e741dfd9346affda403db146e
Atul Gopinathan <atulgopinathan@gmail.com>
2021-06-16 23:32:38 +0530

    Fixed multiple socket and peer connections on theme change
    Earlier, the peer and socket clients were being instantiated
    inside ValidMeet() component. This component, on changing the
    theme while pressing theme toggle button would re-render and
    it will cause the above clients to re-instantiate, therefore
    multiple client objects are created causing a leak and unecessary
    traffic. Fix this by moving the socket and peer client creation
    in Meet component.
    
    I've also used the jsx format of <ValidMeet /> instead of a normal
    function call of "ValidMeet()" inside Meet() component to make sure
    Meet() is not re-rendered when I toggle theme and only ValidMeet
    is called again.

--- frontend/src/Meet.js
+++ frontend/src/Meet.js
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { validate as uuidValidate } from 'uuid';
+import io from 'socket.io-client'
+import  Peer from 'peerjs'
 
 import InvalidMeet from './InvalidMeet'
 import ValidMeet from './ValidMeet'
@@ -8,10 +10,25 @@ const Meet = () => {
     const { meetId } = useParams(); //get the meetId passed in url as param
 
     if(uuidValidate(meetId) === false) {
-        return InvalidMeet();
+        return <InvalidMeet />;
     }
 
-    return ValidMeet(meetId);
+    //leaving the arguemnt of io as blank, this will make it connect to the
+    //"/" by default
+    let socket = io()
+
+    //passing undefined for id, because Peer will automatically create one
+    let peer = new Peer(undefined, {
+        path: '/peerjs',    //we got this from server.js
+        host: '/',  //whatever host this is being hosted on - heroku/local etc
+        port: '5000'    //nodejs server is on 5000, change to 443 in heroku
+    })
+
+    return (
+        <div>
+            <ValidMeet meetId={meetId} socket={socket} peer={peer} />
+        </div>
+    );
 }
 
 export default Meet
--- frontend/src/ValidMeet.js
+++ frontend/src/ValidMeet.js
@@ -1,6 +1,4 @@
 import React, { useContext } from 'react'
-import io from 'socket.io-client'
-import  Peer from 'peerjs'
 
 import VideoSection from './VideoSection'
 import ChatSection from './ChatSection'
@@ -9,29 +7,26 @@ import ThemeButton from './ThemeButton'
 
 import { ThemeContext } from './contexts/ThemeContext'
 
-const ValidMeet = (meetId) => {
+import './ValidMeet.css'
+
+const ValidMeet = ({meetId, socket, peer}) => {
     const { darkMode } = useContext(ThemeContext);
-    console.log(`from ValidMeet: ${darkMode}`)
-
-    //leaving the arguemnt of io as blank, this will make it connect to the
-    //"/" by default
-    let socket = io()
-
-    //passing undefined for id, because Peer will automatically create one
-    let peer = new Peer(undefined, {
-        path: '/peerjs',    //we got this from server.js
-        host: '/',  //whatever host this is being hosted on - heroku/local etc
-        port: '5000'    //nodejs server is on 5000, change to 443 in heroku
-    })
+    
+    let validMeetClass;
+    if(darkMode) {
+        validMeetClass = "valid-meet-section valid-meet-dark";
+    } else {
+        validMeetClass = "valid-meet-section valid-meet-light";
+    }
 
     return (
-        <>
+        <div className={validMeetClass}>
+            <ThemeButton />
             <h2>{`${meetId}`}</h2>
             <VideoSection meetId={meetId} socket={socket} peer={peer} />
-            <ThemeButton />
             <ChatSection />
             <BottomBar />
-        </>
+        </div>
     );
 }
 
